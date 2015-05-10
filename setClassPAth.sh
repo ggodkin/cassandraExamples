1363e27ad2390043ab71fed711e69b6f918fb745
Greg Godkin <grgodkin@yahoo.com>
2015-05-10 14:23:44 -0500

    setClassPAth.sh

---
+++ setClassPAth.sh
@@ -0,0 +1,1 @@
+export CLASSPATH=$CLASSPATH:/home/datastax/cassandra-java-driver-2.1.4/*:/home/datastax/cassandra-java-driver-2.1.4/cassandra-driver-core-2.1.4.jar:/home/datastax/cassandra-java-driver-2.1.4/lib/*:.
